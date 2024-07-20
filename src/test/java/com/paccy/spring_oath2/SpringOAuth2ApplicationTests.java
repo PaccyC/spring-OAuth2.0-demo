38d31af8327bdba69989fc0b858a546ac736704e
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-20 16:21:01 +0200

    initial commit

---
+++ src/test/java/com/paccy/spring_oath2/SpringOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paccy.spring_oath2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
