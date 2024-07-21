fad25b26266ac03e759a561fc05a1c805ca679cc
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-21 23:54:41 +0200

    modified security config

--- src/main/java/com/paccy/spring_oath2/security/SecurityConfig.java
+++ src/main/java/com/paccy/spring_oath2/security/SecurityConfig.java
@@ -1,9 +1,27 @@
-package com.paccy.spring_oath2.security;
+package com.paccy.spring_oauth2.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/login", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .loginPage("/login")
+                );
+        return http.build();
+    }
 }
